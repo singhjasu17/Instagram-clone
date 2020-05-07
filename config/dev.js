c2d9e733932405d7c257d98eb8720ce12efc1555
Harsimran Singh <44256411+singhjasu17@users.noreply.github.com>
2020-05-07 20:26:22 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,9 @@
 module.exports=
 {
-    MONGOURL:"mongodb+srv://Harsimran:3453S7Fe0xOTrda5@instagramclone-oa7ef.mongodb.net/test?retryWrites=true&w=majority" ,
-    SECRET_KEY:"my bday's on 4th jan",
-    api_user:"HARSI",
-    api_key:"griHarsi11",
+    MONGOURL:"your mongo url" ,
+    SECRET_KEY:"your secret key",
+    api_user:"sendgrid user name",
+    api_key:"sengrid password",
     EMAIL:'http://localhost:3000/'
 
 }
